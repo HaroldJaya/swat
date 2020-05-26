3e98490098c78249a1de9ecb7096158e26a4c4f2
Manny <egalli64@gmail.com>
2020-05-26 17:23:54 +0200

    log example added

--- src/main/java/dd/sample/swat/s02/Hello.java
+++ src/main/java/dd/sample/swat/s02/Hello.java
@@ -1,12 +1,17 @@
 package dd.sample.swat.s02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
+    private static Logger LOG = LoggerFactory.getLogger(Hello.class);
+
     @GetMapping("/s02/hello")
     public String hello() {
+        LOG.trace("hello");
         return "Hello";
     }
 }
