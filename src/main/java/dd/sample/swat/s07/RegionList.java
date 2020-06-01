eb088c500246b0140f3b524b8d1c46c9d156ad15
Manny <egalli64@gmail.com>
2020-06-01 15:23:26 +0200

    JPA MySQL example

---
+++ src/main/java/dd/sample/swat/s07/RegionList.java
@@ -0,0 +1,26 @@
+package dd.sample.swat.s07;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegionList {
+    @Autowired
+    private RegionRepository repo;
+
+    private static final Logger LOG = LoggerFactory.getLogger(RegionList.class);
+
+    @GetMapping("/s07/regions")
+    public String list(Model model) {
+        LOG.trace("listing all regions");
+
+        System.out.println(repo.count());
+
+        return "/s07/regions";
+    }
+
+}
