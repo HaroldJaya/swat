384a1ee921f5d8277b91f220dd8af3eb34d5e021
Manny <egalli64@gmail.com>
2020-06-01 15:17:56 +0200

    minor cleanup

--- src/main/java/dd/sample/swat/SwatApplication.java
+++ src/main/java/dd/sample/swat/SwatApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwatApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SwatApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SwatApplication.class, args);
+    }
 }
